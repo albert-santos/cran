77eddc82df4fb0b75dbb216b48e9537da75031a5
Albert Santos <alberteinsteincds@gmail.com>
2022-02-18 19:31:13 -0300

    obter planilhas de uma execução específica

--- Smalls_position.m
+++ Smalls_position.m
@@ -1,4 +1,4 @@
-function [EnbsPosition] = Smalls_position(Micros)
+function [EnbsPosition] = Smalls_position(Micros, Sim)
 
 horas = size(Micros, 1);
 total_smalls = size(Micros, 2);
@@ -6,9 +6,9 @@ total_smalls = size(Micros, 2);
 for j = 1:horas
     a = 1;
     for l = 1:total_smalls
-        if (Micros(j,l).D == 1)
-            EnbsPosition(a,1,j) = Micros(j,l).X;
-            EnbsPosition(a,2,j) = Micros(j,l).Y;
+        if (Micros(j,l,Sim).D == 1)
+            EnbsPosition(a,1,j) = Micros(j,l,Sim).X;
+            EnbsPosition(a,2,j) = Micros(j,l,Sim).Y;
             a = a + 1;
         end
     end
--- Users_position.m
+++ Users_position.m
@@ -4,9 +4,9 @@ b = length(UE);
 for j = 1:24
     a = 1;
     for l = 1:b
-        if (UE(l).M == j)
-            User(a,1,j) = UE(l).X;
-            User(a,2,j) = UE(l).Y;
+        if (UE(Sim, l).M == j)
+            User(a,1,j) = UE(Sim, l).X;
+            User(a,2,j) = UE(Sim, l).Y;
             a = a + 1;
         end
     end
