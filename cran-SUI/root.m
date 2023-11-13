38174241d82e4859ddc21219c75c587eac2c4036
Albert Santos <alberteinsteincds@gmail.com>
2023-11-13 19:27:16 -0300

    integrando com o HDSO

--- cran-SUI/root.m
+++ cran-SUI/root.m
@@ -1,4 +1,4 @@
-function [saida, tempo_execucao, Micros, U, small_cell_status] = root(Usuarios, SmallCells, MacroCells)
+function [saida, tempo_execucao, Micros, U, small_cell_status] = root(Usuarios, SmallCells, MacroCells, optimization_model)
 
 % -------------------------------------------------------------------------
 % Parte III --> Criação do Cenário para etapa de otimização
@@ -20,17 +20,28 @@ for j = 1:24
     [Us1, ~] = ConexaoUsM(User, Macro);
     % Conexão Usuário/Small
     [Us1, S1] = ConexaoUs(Us1, Small); % Usuários e Small
-%     [Us1, S1] = ConexaoUs(User, Small); % Usuários e Small
     [M1] = Media(Us1); % M = [DataRate SINR > DR <DR UD]
     [V21] = Media_M(S1, Us1); %S1 - SmallCells com nº(S1.U) e indices(S1.VU) de usuarios conectados
     
 %     % Verifica as Small próximas ao usuário
 %     [User, ~] = NearbySmalls(User, Small);
+
     
-    [saida(j,:), numero_SC(j, :), Micros(j, :), small_cell_status(j,:)] = SA_algorithm (V21, User, Small, Macro);
-    fprintf('Implementando SA para a hora #%d!\n', j);
-
-
+    if strcmpi(optimization_model,'SA')
+        [saida(j,:), numero_SC(j, :), Micros(j, :), small_cell_status(j,:)] = SA_algorithm (V21, User, Small, Macro);
+        fprintf(' SA executado para a hora #%d!\n', j);
+    end
+    
+    if strcmpi(optimization_model,'HDSO')
+        
+        % Cálculo HDSO
+        [ON, OFF(j,:)] = HDSO_algorithm(Macro, S1,Us1,V21);
+        % Quantidade de micros que devem ser ligadas
+        SM(j, :) = ON;
+    
+        [saida(j,:), Micros(j,:), small_cell_status(j,:)] = Seleciona_RRH_HDSO(User, OFF(j,:), Macro, Small);
+        fprintf('HDSO executado para a hora #%d!\n', j);
+    end
     %Sn(24x4)
     
     clear User;
